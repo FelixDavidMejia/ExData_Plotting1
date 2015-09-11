38464b4f9de2967ecd329defc1883bdf36295a36
Félix David Mejía Mejía <fmejia01@gmail.com>
2015-09-11 12:36:39 -0400

    Adding plot1.R

---
+++ plot1.R
@@ -0,0 +1,13 @@
+# Data Science Specialization
+# Exploratory Data Analysis: Course Project #1
+## Program: plot1.R
+## Student: Félix D. Mejía
+
+## Abstract
+###### This assignment uses data from "Individual household electric power consumption Data Set", which includes measurements of electric power consumption in one household with a one-minute sampling rate over a period of almost 4 years. Different electrical quantities and some sub-metering values are available.
+###### This program will generate file "plot1.png", a frequency histogram of Global Active Power measured in KiloWatts.
+
+
+
+
+
