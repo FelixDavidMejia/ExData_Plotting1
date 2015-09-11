b19ff3388b66933587cf339cedf26a397b29662f
Félix David Mejía Mejía <fmejia01@gmail.com>
2015-09-11 12:38:17 -0400

    Changes to plot1.R

--- plot1.R
+++ plot1.R
@@ -4,8 +4,12 @@
 ## Student: Félix D. Mejía
 
 ## Abstract
-###### This assignment uses data from "Individual household electric power consumption Data Set", which includes measurements of electric power consumption in one household with a one-minute sampling rate over a period of almost 4 years. Different electrical quantities and some sub-metering values are available.
-###### This program will generate file "plot1.png", a frequency histogram of Global Active Power measured in KiloWatts.
+## This assignment uses data from "Individual household electric power consumption Data Set",
+## which includes measurements of electric power consumption in one household with a one-minute
+## sampling rate over a period of almost 4 years. Different electrical quantities and some
+## sub-metering values are available.
+
+## This program will generate file "plot1.png", a frequency histogram of Global Active Power measured in KiloWatts.
 
 
 
